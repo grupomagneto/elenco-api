ec5bc0a9ff3ad6e333c4b4b930a4a619eb43bfcb
vini-goulart <Vgb808285>
2016-08-25 14:46:22 -0300

    Com CCS temporário

--- meu_modelo_favorito/action_select.php
+++ meu_modelo_favorito/action_select.php
@@ -4,6 +4,7 @@ date_default_timezone_set('America/Sao_Paulo');
 	$winner_candidate_ID = $_POST['candidate_elenco_ID'];
 	$loser_candidate_ID = $_POST['loser_candidate_ID'];
 	$game_ID = $_POST['game_ID'];
+	$facebook_ID = $_POST['facebook_ID'];
 	$vote_time_start = $_POST['vote_time_start'];
 	$page = $_POST['page'];
 	$total_pages = $_POST['total_pages'];
@@ -13,7 +14,7 @@ date_default_timezone_set('America/Sao_Paulo');
 	$vote_date = date('Y-m-d');
 	$vote_time = date('H:i:s');
 	$vote_delay = microtime(true) - $vote_time_start;
-	$sql2 = "INSERT INTO tb_votes (game_ID, level, winner_candidate_ID, loser_candidate_ID, vote_date, vote_time, vote_delay) VALUES ('$game_ID','$write_level','$winner_candidate_ID','$loser_candidate_ID','$vote_date','$vote_time','$vote_delay')";
+	$sql2 = "INSERT INTO tb_votes (game_ID, voter_facebook_ID, level, winner_candidate_ID, loser_candidate_ID, vote_date, vote_time, vote_delay) VALUES ('$game_ID','$facebook_ID','$write_level','$winner_candidate_ID','$loser_candidate_ID','$vote_date','$vote_time','$vote_delay')";
 	mysqli_query($link2, $sql2);
 	if ($page <= $total_pages) {
 		header("Location: pagination.php?game_ID=$game_ID&page=$page&level=$view_level");
